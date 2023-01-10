068f4f50a2094ae72991f6687d187315e4a8cabf
jiw0000 <112916241+jiw0000@users.noreply.github.com>
2023-01-10 18:39:34 +0900

    Add files via upload

---
+++ Chapter04_01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	char first[10];
+	char last[20];
+
+	printf("¿Ã∏ß : ");
+	scanf("%s", &last);
+
+	printf("º∫ : ");
+	scanf("%s", &first);
+
+	printf(" %s%s", first, last);
+
+	return 0;
+}
